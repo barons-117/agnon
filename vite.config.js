232c11bd8fa3ba6d24e70ba04977df3354669a61
Erez Baron <erezbaron@Erezs-iMac-Blue-2.local>
2026-03-09 23:09:52 +0200

    עדכון base לדומיין agnon.net

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ⚠️ שנה את 'YOUR-REPO-NAME' לשם ה-repository שלך ב-GitHub
-// לדוגמה: אם הכתובת היא github.com/john/shay-agnon → base: '/shay-agnon/'
 export default defineConfig({
   plugins: [react()],
-  base: '/agnon/',
+  base: '/',
 })
